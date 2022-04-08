d48498d722218fcedf0c8ac4ea79f826b0db6152
Elias_Silva <inboxsilva1998@gmail.com>
2022-04-08 15:25:06 -0300

    Add Exercise 6 month 1 week 3

---
+++ M1S03/M1S03-ex6/index.js
@@ -0,0 +1,5 @@
+sobreNome = prompt("Qual seu último sobrenome?");
+sobreNomeMaisculo = sobreNome.toUpperCase();
+
+
+alert("Olá, " + sobreNomeMaisculo + " Seu sobrenome contém " + sobreNome.length + " letras");
