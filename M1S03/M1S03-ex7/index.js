ae448f1c939e659ab6d314e7a1eb5009a8be5bd8
Elias_Silva <inboxsilva1998@gmail.com>
2022-04-08 15:43:06 -0300

    Add Exercise 7 month 1 week 3

---
+++ M1S03/M1S03-ex7/index.js
@@ -0,0 +1,6 @@
+anoNascimento = Number(prompt("que ano voce nasceu?"));
+anoAtual = Number(prompt("que ano nós estamos?"));
+calculoIdade = anoAtual - anoNascimento
+
+
+alert("Você tem ou vai fazer " + calculoIdade + " anos este ano");
