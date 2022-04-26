ef995a96fd73338162eedb116e0a2275b7bb45bf
Elias_Silva <inboxsilva1998@gmail.com>
2022-04-26 13:34:34 -0300

    Set dynamic list for content items

--- M1S08/ng-food/src/app/content/drink-content/drink-content.component.ts
+++ M1S08/ng-food/src/app/content/drink-content/drink-content.component.ts
@@ -1,11 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Inotificacao {
+  titulo: string
+}
+
 @Component({
   selector: 'ngf-drink-content',
   templateUrl: './drink-content.component.html',
   styleUrls: ['./drink-content.component.scss']
 })
 export class DrinkContentComponent implements OnInit {
+
+  listaDeNotificacoes: Inotificacao[] = [
+    {
+      titulo: "Laranjada"
+    },
+    {
+      titulo: "Limão"
+    },
+    {
+      titulo: "Vitamina de Tuti-Fruti"
+    },
+    {
+      titulo: "Coca-cola"
+    },
+    {
+      titulo: "Maracujá"
+    }
+  ]
 
   constructor() { }
 
--- M1S08/ng-food/src/app/content/food-content/food-content.component.ts
+++ M1S08/ng-food/src/app/content/food-content/food-content.component.ts
@@ -1,11 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Inotificacao {
+  titulo: string
+}
+
 @Component({
   selector: 'ngf-food-content',
   templateUrl: './food-content.component.html',
   styleUrls: ['./food-content.component.scss']
 })
 export class FoodContentComponent implements OnInit {
+
+  listaDeNotificacoes: Inotificacao[] = [
+    {
+      titulo: "Batata Frita"
+    },
+    {
+      titulo: "Hamburguer"
+    },
+    {
+      titulo: "Pizza"
+    },
+    {
+      titulo: "Esfira"
+    },
+    {
+      titulo: "Torta"
+    }
+  ]
 
   constructor() { }
 
