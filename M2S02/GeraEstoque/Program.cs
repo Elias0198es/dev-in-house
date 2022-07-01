8b37a14bf6b8775b7a8f5391c4a96b7e1b9e0600
Elias_Silva <inboxsilva1998@gmail.com>
2022-07-01 14:50:45 -0300

    Exercicio 6 - Semana 2

--- M2S02/GeraEstoque/Program.cs
+++ M2S02/GeraEstoque/Program.cs
@@ -5,7 +5,12 @@ namespace GeraEstoque
     {
         public static void Main()
         {
-            
+            int opcao;
+
+            System.Console.WriteLine("Seja bem vindo ao GeraEstoque 1.0");
+            System.Console.WriteLine(" 1 Cadastrar produto \r\n 2 Consultar produto \r\n 3 Modificar produto \r\n 4 Excluir produto \r\n 0 Sair \r\n Digite a opção:");
+            opcao = Convert.ToInt32(Console.ReadLine());
+
         }
     }
 }
